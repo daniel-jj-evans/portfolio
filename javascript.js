2fdf31dad1022c5d0a77bb50fd53ba145ef4169f
daniel evans <daniel.jj.evans1@gmail.com>
2020-11-06 11:34:22 +0000

    done

---
+++ javascript.js
@@ -0,0 +1,46 @@
+
+
+//var elementFadeIn = document.getElementById("")
+
+//var opacity = 0;
+
+/*var fadeIn = function(){
+    if(opacity <= 1.0){
+        elementFadeIn.style.opacity = opacity;
+    } else {
+        clearInterval(timer);
+    }
+    opacity += 0.1;
+
+}
+
+var timer = window.setInterval(fadeIn, 50);
+*/
+
+
+const showMoreProjects = () =>{
+    let moreProjects = document.getElementById("project-sect");
+        if (moreProjects.style.visibility === "hidden") {
+            moreProjects.setAttribute("style","visibility:visible", "height:100px");
+        } else {
+            moreProjects.setAttribute("style","visibility:hidden", "height:0vh") ;
+        }
+};
+
+//$(document).ready(function(){
+//    $(".burgerNav").on("click",function(){
+//
+//        $("ul").toggleClass("open");
+//
+//    });
+//});
+
+const showBurgerNav = () =>{
+    let burgerButtons = document.getElementById("burgerButtons");
+
+    if (burgerButtons.style.display === "none"){
+        burgerButtons.style.display= "block";
+    }else{
+        burgerButtons.style.display= "none";
+    }
+}
